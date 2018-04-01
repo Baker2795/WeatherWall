17148ca5983f9d2fc6a4fa268f8929401eedfd64
John Baker <john@b4k3r.com>
2018-03-31 21:37:46 -0400

    Initial Commit

---
+++ WeatherWall/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherWall
+//
+//  Created by John Baker on 3/31/18.
+//  Copyright © 2018 B4k3R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
